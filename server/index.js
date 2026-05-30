6a0c7b2000312882e9cc28212fa8084ab6ea19f7
s354774 <s354774@studenti.polito.it>
2026-05-30 19:19:53 +0200

    feat(server): index.js config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 // imports
 import express from "express";
-
+import morgan from "morgan";
+import cors from "cors";
+import { sessionMiddleware, passport } from "./lib/auth";
+import sessionRoutes from "./routes/sessionRoutes.js";
+import gameRoutes from "./routes/userRoutes.js";
 // init express
 const app = new express();
-const port = 3001;
+const PORT = 3001;
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(sessionRoutes);
+app.use(gameRoutes);
+
+app.use((err, req, res, next) => { 
+  console.error(err)
+  res.status(500).json({ error: "Internal Server Error" });
+});
 
 // activate the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+export default app;
