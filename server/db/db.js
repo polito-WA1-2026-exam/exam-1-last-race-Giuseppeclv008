def1664811d1bbb3ae42bc90181a75705c6ae64e
s354774 <s354774@studenti.polito.it>
2026-05-30 17:28:54 +0200

    feat(server): implementation of the db main connection functions

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,39 @@
 "use strict";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+export const DB_PATH = "server/db/db.js";
 
-let dbPromise;
+// Singleton pattern to ensure that only one database connection is established
+// If the database connection has already been established, it returns the existing promise
+
+let db;
 
 export function getDb() {
-  if (!dbPromise) {
-    dbPromise = new sqlite3.Database("lastrace.db", 
+  if (!db) {
+    db = new sqlite3.Database(DB_PATH, 
         (err) =>{if (err) throw err;});
+    db.run("PRAGMA foreign_keys = ON");
   }
   return dbPromise;
+}
+
+export function dbAll(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        getDb().all(sql, params, (err, rows) => { (err) ? reject(err) : resolve(rows); });
+    });
+}
+
+export function dbGet(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        getDb().get(sql, params, (err, row) => { (err) ? reject(err) : resolve(row); });
+    });
+}
+
+export function dbRun(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        getDb().run(sql, params, function(err) {
+             if (err) reject(err);
+             else resolve({lastId: this.lastID, changes: this.changes});
+        });
+    });
 }
